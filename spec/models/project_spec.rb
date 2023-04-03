ee00f0edf50db071deaf02fcf7afe073c2c64da6
mocchirihamu <tech.0813.1114@gmail.com>
2023-04-03 17:00:41 +0900

    projectモデルの単体テストコードの実装

---
+++ spec/models/project_spec.rb
@@ -0,0 +1,38 @@
+require 'rails_helper'
+
+RSpec.describe Project, type: :model do
+  before do
+    @project = FactoryBot.build(:project)
+  end
+
+  context '依頼案件が投稿できる場合' do
+    it 'category_id、limit、detail、user_idが存在すれば投稿できる' do
+      expect(@project).to be_valid
+    end
+    it 'suppulementが空でも投稿できる' do
+      @project.suppulement = ''
+      expect(@project).to be_valid
+    end
+    it 'imageが空でも投稿できる' do
+      @project.image = nil
+      expect(@project).to be_valid
+    end
+  end
+  context '商品出品できない場合' do
+    it 'category_idが空では登録できない' do
+      @project.category_id = 1
+      @project.valid?
+      expect(@project.errors.full_messages).to include("Category can't be blank")
+    end
+    it 'limitが空では登録できない' do
+      @project.limit = ""
+      @project.valid?
+      expect(@project.errors.full_messages).to include("Limit can't be blank")
+    end
+    it 'detailが空では登録できない' do
+      @project.detail = ""
+      @project.valid?
+      expect(@project.errors.full_messages).to include("Detail can't be blank")
+    end
+  end
+end
